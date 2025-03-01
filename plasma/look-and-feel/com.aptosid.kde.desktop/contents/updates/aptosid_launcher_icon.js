978f57d6f3e832c8f67fbd7baa7cbf38bb63a94f
kelmo <kelvmod@gmail.com>
2025-03-01 10:03:48 +1000

    plasma: add updates/aptosid_launcher_icon.js to look-and-feel to default to emblem-aptosid launcher icon.
    * plasma: add updates/aptosid_launcher_icon.js to look-and-feel to
      default to emblem-aptosid launcher icon.
    * lxqt: use aptosid emblem as mainmenu icon.
    * Install *.svg to hicolor/scalable/emblems.

---
+++ plasma/look-and-feel/com.aptosid.kde.desktop/contents/updates/aptosid_launcher_icon.js
@@ -0,0 +1,35 @@
+/* Utility functions from https://develop.kde.org/docs/plasma/scripting/examples/. */
+
+function forEachWidgetInContainmentList(containmentList, callback) {
+    for (var containmentIndex = 0; containmentIndex < containmentList.length; containmentIndex++) {
+        var containment = containmentList[containmentIndex];
+
+        var widgets = containment.widgets();
+        for (var widgetIndex = 0; widgetIndex < widgets.length; widgetIndex++) {
+            var widget = widgets[widgetIndex];
+            callback(widget, containment);
+            if (widget.type === "org.kde.plasma.systemtray") {
+                systemtrayId = widget.readConfig("SystrayContainmentId");
+                if (systemtrayId) {
+                    forEachWidgetInContainmentList([desktopById(systemtrayId)], callback)
+                }
+            }
+        }
+    }
+}
+
+function forEachWidget(callback) {
+    forEachWidgetInContainmentList(desktops(), callback);
+    forEachWidgetInContainmentList(panels(), callback);
+}
+
+forEachWidget(function(widget, containment) {
+    if (widget.type != "org.kde.plasma.kicker" && widget.type != "org.kde.plasma.kickoff")
+         return;
+
+    widget.currentConfigGroup = ["General"];
+    print("Icon for " + widget.type + ": " + widget.readConfig("icon"));
+    // If using the builtin hardcoded default, switch to the branding icon
+    if (widget.readConfig("icon") == "")
+        widget.writeConfig("icon", "emblem-aptosid")
+});
